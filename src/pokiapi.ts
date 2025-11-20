8c4a4067f8335645a75f3704aeaff403edaf54b7
yiqiyihui99 <ianwooduffy.99@gmail.com>
2025-11-20 14:12:05 -0800

    Added basic pokiAPI fetch and types logic

---
+++ src/pokiapi.ts
@@ -0,0 +1,41 @@
+export class PokeAPI {
+    private static readonly baseURL = "https://pokeapi.co/api/v2";
+  
+    constructor() {}
+  
+    async fetchLocations(pageURL?: string): Promise<ShallowLocations> {
+        try {
+            const locations = await fetch(pageURL || `${PokeAPI.baseURL}/location-area/`);
+            return await locations.json(); 
+        } catch (e) {
+            throw new Error(`Failed to fetch locations: ${e}`);
+        }
+    }
+  
+    async fetchLocation(locationName: string): Promise<Location> {
+      try {
+        const location = await fetch(`${PokeAPI.baseURL}/location-area/${locationName}`);
+        return await location.json();
+      } catch (e) {
+        throw new Error(`Failed to fetch location: ${e}`);
+      }
+    }
+  }
+  
+  export type ShallowLocations = {
+    count: number;
+    next: string | null;
+    previous: string | null;
+    results: {
+        name: string;
+        url: string;
+    }[];
+  };
+  
+  export type Location = {
+    id: number;
+    name: string;
+    pokemon_encounters: {
+      pokemon: { name: string; url: string };
+    }[];
+  };
